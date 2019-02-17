7c7c3b58ce153cd165253335e5882e52029c7f4c
Veck Hsiao <fbukevin@gmail.com>
2019-02-17 22:27:00 +0800

    Initial Commit

---
+++ CodeWarPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeWarPractice
+//
+//  Created by Veck  Hsiao on 2019/2/17.
+//  Copyright © 2019 Sanity. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
