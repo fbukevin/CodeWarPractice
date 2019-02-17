81b5eadab9dd8fc2f3212d2b685957c1dc9faf3d
Veck Hsiao <fbukevin@gmail.com>
2019-02-18 01:19:36 +0800

    Add best answer of ConsecutiveString

--- SolutionExample/CosecutiveString.swift
+++ SolutionExample/CosecutiveString.swift
@@ -10,26 +10,13 @@ import Foundation
 
 public class ConsecutiveString {
   static func longestConsec(_ strarr: [String], _ k: Int) -> String {
-    print(strarr, k)
-    if(strarr.count == 0 || k > strarr.count || k <= 0) { return "" }
-
-    var longest = (word: "", length: 0)
-
-    for (index, _) in strarr.enumerated() {
-      if(index+k > strarr.count){ break }
-
-      var combine = ""
-      for i in 0..<k {
-        combine = "\(combine)\(strarr[index+i])"
-      }
-
-
-      if(combine.characters.count > longest.length){
-        longest.word = combine
-        longest.length = combine.characters.count
-      }
+    var longest: String = ""
+    let n = strarr.count
+    if n == 0 || k > n || k <= 0 {return longest}
+    for i in 0..<(n - k + 1) {
+      let str: String = strarr[i..<i + k].joined(separator: "")
+      if str.characters.count > longest.characters.count {longest = str}
     }
-
-    return longest.word
+    return longest
   }
 }
